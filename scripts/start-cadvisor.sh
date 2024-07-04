f5adf408076839cdfde1f2515bb20815dae0a3a9
G3 <tuxgal@Baymax.area51>
2024-07-04 12:48:13 -0700

    feat: Use capsh to start the cadvisor binary.
    Somehow the results of setcap do not seem to get persisted in the docker
    image. Instead at runtime, we will use capsh to run cadvisor as our
    desired user and with the desired capabilities set.

--- scripts/start-cadvisor.sh
+++ scripts/start-cadvisor.sh
@@ -7,14 +7,35 @@ set_umask() {
     umask 0002
 }
 
+validate_prereqs() {
+    if [ $(id -u) -ne 0 ]; then
+        echo "Cannot start this container as non-root!"
+        echo "Ability to switch user to cadvisor requires launching as root!"
+        echo "Need to run this container as root, however running as ${USER:?} [$(id -a)]"
+        exit 1
+    fi
+
+    if ! capsh --has-b=cap_syslog; then
+        echo "You are attempting to run the container without SYSLOG capability!"
+        echo "Without SYSLOG capability, /dev/kmsg cannot be accessed!"
+        echo "Please run with --cap-add=SYSLOG instead!"
+        exit 1
+    fi
+}
+
 start_cadvisor() {
     echo "Starting cAdvisor ..."
     echo
 
-    exec cadvisor \
-        -logtostderr \
-        "$@"
+    exec capsh \
+        --keep=1 \
+        --user=cadvisor \
+        --inh=cap_syslog \
+        --addamb=cap_syslog \
+        -- \
+        -c cadvisor -logtostderr "$@"
 }
 
 set_umask
+validate_prereqs
 start_cadvisor "$@"
